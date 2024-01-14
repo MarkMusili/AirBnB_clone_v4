09c35c6762cc174457c29363cb273325f47a77ae
Ote Ian <oteian9@gmail.com>
2024-01-14 22:53:44 +0300

    Filter places by amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -30,29 +30,34 @@ $(function () {
     success: (data) => {
       data.forEach((place) =>
         $('section.places').html(
-                    `<article>
-                <div class="title_box">
-                  <h2>${place.name}</h2>
-                  <div class="price_by_night">${place.price_by_night}</div>
-                </div>
-                <div class="information">
-                  <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''
-                    }</div>
-                      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''
-                    }</div>
-                      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''
-                    }</div>
-                </div>
-                    <div class="description">
-                  ${place.description}
-                    </div>
-              </article>
-          `
+                      `<article>
+                  <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">${place.price_by_night}</div>
+                  </div>
+                  <div class="information">
+                    <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''
+                      }</div>
+                        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''
+                      }</div>
+                        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''
+                      }</div>
+                  </div>
+                      <div class="description">
+                    ${place.description}
+                      </div>
+                </article>
+            `
         ));
     },
     dataType: 'json',
     error: function (error) {
       console.error('Error:', error);
     }
+  });
+  $('button').click(function () {
+    $.post('http://127.0.0.1:5001/api/v1/places_search', {
+        
+    })
   });
 });
